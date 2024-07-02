0d07b21041925e110f9b5f09d858a2a5408b468a
Dustin Lee <dustin.jason.lee@gmail.com>
2024-07-02 05:47:14 -0500

    add vercel.json config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    __APP_ENV__: 'production'
-  }
+  plugins: [react()]
 })
