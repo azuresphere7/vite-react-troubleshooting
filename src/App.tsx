ab8c8f13040e6087e84fdb26839437940b324a36
Dustin Lee <dustin.jason.lee@gmail.com>
2024-07-02 05:38:17 -0500

    add Not Found Page

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import './App.css'
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+import NotFoundPage from './pages/NotFound';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path='*' element={<NotFoundPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
