a294a45362fd9180f06702485db6ac38b77903e6
Dustin Lee <dustin.jason.lee@gmail.com>
2024-07-02 05:35:35 -0500

    add react-router-dom

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+export default function AboutPage() {
+  return (
+    <h1>This is About Page.</h1>
+  )
+}
---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,5 @@
+export default function ContactPage() {
+  return (
+    <h1>This is Contact Page.</h1>
+  )
+}
