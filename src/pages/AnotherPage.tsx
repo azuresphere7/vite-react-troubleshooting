35d4f87d1aa451be9f6416186daf94c6be662b7c
Morgan Mastrangelo <mmastrangelo1120@gmail.com>
2024-08-01 11:17:08 -0500

    Request for the evaluation

--- src/pages/AnotherPage.tsx
+++ src/pages/AnotherPage.tsx
@@ -1,5 +1,7 @@
-export default function AnotherPage() {
+function AnotherPage() {
   return (
     <h1>Yay! Now this page works and shows us the component!</h1>
   )
 }
+
+export default AnotherPage;
